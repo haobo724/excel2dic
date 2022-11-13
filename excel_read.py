80405ae6a413a8f5b29ad6638544c9b872ca64b5
haobo724 <s724724@gmail.com>
2022-11-14 00:13:59 +0100

    first commit

---
+++ excel_read.py
@@ -0,0 +1,75 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+
+# Define input out
+path = Path(os.path.dirname(__file__))
+path_in = os.path.join(path, "FP")
+path_out = os.path.join(path, "Results")
+if not os.path.exists(path_out):
+    os.makedirs(path_out)
+
+class Excel2Dic:
+    def __init__(self, csv_name='People_Asia'):
+        self.xls_table_key = \
+            pd.read_excel(os.path.join(path_in, f"{csv_name}.xlsx"), sheet_name=None, skiprows=0).keys()
+        t = list(self.xls_table_key)[0]
+        self.xls_table = pd.read_excel(os.path.join(path_in, f"{csv_name}.xlsx"), sheet_name=None, skiprows=0)[t]
+
+        self.contoury_name = list(self.xls_table.iloc[:, 0])
+        self.dict = {}
+        self.years, self.Nr_years_apart = [0], 0
+        self._get_years()
+        self._generate_dic()
+        self.name = csv_name
+    def _check_name_validation(self, name: str):
+        if name in self.contoury_name:
+            return True
+        else:
+            print(f'{name} is not in {self.name}table')
+            return False
+
+    def _check_year_validation(self, year: str):
+        if int(year) in self.years:
+            return True
+        else:
+            return 0
+
+    def _generate_dic(self):
+        contoury_dict = {}
+        conter = 1
+        for y in self.years:
+            for i in range(self.xls_table.__len__()):
+                value = list(self.xls_table.iloc[i])[conter]
+                try:
+                    value = float(value)
+                except ValueError:
+                    value=0
+
+                contoury_dict.setdefault(self.contoury_name[i], value)
+
+            conter += 1
+            self.dict.setdefault(str(y), contoury_dict)
+            contoury_dict = {}
+
+    def get_value(self, year: str, name: str):
+        if not self._check_year_validation(year):
+            return 0
+        if self._check_name_validation(name):
+
+            return self.dict[year][name]
+        else:
+            return 0
+
+    def get_year_value(self, year: str):
+        if not self._check_year_validation(year):
+            return 0
+
+        return self.dict[year]
+
+    def _get_years(self):
+        self.years = list(self.xls_table.iloc[:0])[1:]
+        self.Nr_years_apart = self.years[-1] - self.years[0]
+
+
---
+++ write.py
@@ -0,0 +1,74 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+from excel_read import Excel2Dic, path_out
+
+Aluminium_Production_Asia = Excel2Dic('Aluminium_Production_Asia')
+Cement_Production_Asia = Excel2Dic('Cement_Production_Asia')
+Copper_Production_Asia = Excel2Dic('Copper_Production_Asia')
+GDP_Asia = Excel2Dic('GDP_Asia')
+Paper_Production_Asia = Excel2Dic('Paper_Production_Asia')
+People_Asia = Excel2Dic('People_Asia')
+Steel_Production_Asia = Excel2Dic('Steel_Production_Asia')
+
+xls_collection = [Aluminium_Production_Asia, Cement_Production_Asia, Copper_Production_Asia, GDP_Asia,
+                  Paper_Production_Asia, People_Asia, Steel_Production_Asia]
+xls_name = ['Aluminium_Production_Asia', 'Cement_Production_Asia', 'Copper_Production_Asia', 'GDP_Asia',
+            'Paper_Production_Asia', 'People_Asia', 'Steel_Production_Asia']
+
+
+def search_value(xls_dic, country):
+    key = []
+    for forecast_year in range(1960, 2022):
+        value = xls_dic.get_value(str(forecast_year), country)
+        key.append(value)
+
+    return np.array(key)
+
+
+def start(country):
+    column_names_aps = []
+    for forecast_year in range(1960, 2022):
+        column_names_aps.append(forecast_year)
+
+    people = search_value(xls_collection[5], country)
+
+    alum = search_value(xls_collection[0], country)
+    alum_people = alum / people
+
+    Cement = search_value(xls_collection[1], country)
+    Cement_people = Cement / people
+
+    Copper = search_value(xls_collection[2], country)
+    Copper_people = Copper / people
+
+    GDP = search_value(xls_collection[3], country)
+
+    Paper = search_value(xls_collection[4], country)
+    Paper_people = Paper / people
+
+    Steel = search_value(xls_collection[6], country)
+    Steel_people = Steel / people
+
+    col_content_aps = [column_names_aps, people, GDP, alum, alum_people, Cement, Cement_people,
+                       Copper, Copper_people, Paper, Paper_people, Steel, Steel_people]
+    prognosis_aps = pd.DataFrame(col_content_aps)
+
+    prognosis_aps.insert(0, 'Year', ['Year', 'People', 'GDP', 'alum',
+                                     'alum_people', 'Cement',
+                                     'Cement_people', 'Copper',
+                                     'Copper_people', 'Paper',
+                                     'Paper_people', 'Steel',
+                                     'Steel_people'])
+
+    return prognosis_aps
+outfile=os.path.join(path_out, f'sum.xlsx')
+with pd.ExcelWriter(outfile) as writer:
+
+    for country in People_Asia.contoury_name:
+        result = start(country)
+        result.to_excel(writer,sheet_name=f'{country}', index=False, header=False)
+
+
